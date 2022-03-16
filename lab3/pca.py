9565195d7851ced10702170af141fd1a1585fc50
luapicella <apc.luigi@gmail.com>
2022-03-16 23:17:13 +0100

    update pca

--- lab3/pca.py
+++ lab3/pca.py
@@ -1,29 +1,33 @@
 import numpy as np
+from utils import vcol
 
 class PCA:
     
     def __init__(self, n_component):
         self.n_component = n_component
+
+    def __initFit(self, dataset):
+       #compute dataset mean
+       self.mu = dataset.mean(axis=1)
+       self.mu = vcol(self.mu, self.mu.size)
+       #return center data
+       return dataset - self.mu
+       
     
     def fit(self, dataset):
         if self.n_component > dataset.shape[0]:
                 raise Exception("n_componet must be <= of number of feature") 
-        try:
-            # compute covariance matrix
-            C =  np.dot(dataset,dataset.T) / float(dataset.shape[1])
-            # compute eigenvector and eigenvalue
-            s, U = np.linalg.eigh(C)
-            # first n_component element
-            self.P = U[:, ::-1][:, 0:self.n_component]
-        except:
-            print("Something went wrong")
+        
+        dataset = self.__initFit(dataset)
+        # compute covariance matrix
+        C =  np.dot(dataset,dataset.T) / float(dataset.shape[1])
+        # compute eigenvector and eigenvalue
+        s, U = np.linalg.eigh(C)
+        # first n_component element
+        self.P = U[:, ::-1][:, 0:self.n_component]
+       
 
     def trasform(self, dataset):
-        try:
-            dataset_trasformed = np.dot(self.P.T, dataset)
-        except:
-            print("Something went wrong")
-        else:
-            return dataset_trasformed
+        return  np.dot(self.P.T, dataset)
 
 
